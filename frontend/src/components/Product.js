cce9bc47b69594ec2dacf23b2dff1339c94f406a
Larisa Kosareva <kos.lv@yandex.com>
2022-03-30 12:41:31 +0200

    Rating component added, css for h3 modified

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Card } from 'react-bootstrap'
+import Rating from '../components/Rating'
 
 function Product({ product }) {
     return (
@@ -11,13 +12,16 @@ function Product({ product }) {
             <Card.Body>
                 <a href={`/product/${product._id}`}>
                     <Card.Title as="div">
-                        <strong>{product.name}</strong>
+                        {product.name}
                     </Card.Title>
                 </a>
 
                 <Card.Text as="div">
                     <div className="my-3">
-                        {product.rating} from {product.numReviews} reviews
+                        <Rating value={product.rating} 
+                                text={`${product.numReviews} reviews`}
+                                color={`#f8e825`}
+                        />
                     </div>
                 </Card.Text>
 
