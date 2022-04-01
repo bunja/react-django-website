54e9e7ed1944c870a8642b6c1a3e59d05df0da6f
Larisa Kosareva <kos.lv@yandex.com>
2022-04-01 15:06:13 +0200

    views for product,products gettin data from db, image config fixed

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('api/', include('base.urls'))
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_roote=settings.MEDIA_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
