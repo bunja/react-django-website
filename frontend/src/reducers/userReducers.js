4ec3e8b057129c6628c14e70e210a7288bb49333
Larisa Kosareva <kos.lv@yandex.com>
2022-04-19 12:41:33 +0200

    admin screen, reducer, action,  wip

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -19,6 +19,12 @@ import {
     USER_UPDATE_PROFILE_FAIL,
     USER_UPDATE_PROFILE_RESET,
 
+    USER_LIST_REQUEST,
+    USER_LIST_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_RESET,
+
+
 
 
 } from '../constants/userConstants'
@@ -109,6 +115,28 @@ export const userUpdateProfileReducer = (state = {user: {}}, action) => {
             return { loading: false, error: action.payload }
 
         case USER_UPDATE_PROFILE_RESET:
+            return {}
+        
+        default:
+            return state
+    }
+}
+
+export const userListReducer = (state = {users: []}, action) => {
+    switch (action.type) {
+        case USER_LIST_REQUEST:
+            return { loading: true }
+
+        case USER_LIST_SUCCESS:
+            return {
+                loading: false,
+                users: action.payload
+            }
+
+        case USER_LIST_FAIL:
+            return { loading: false, error: action.payload }
+
+        case USER_LIST_RESET:
             return {}
         
         default:
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -8,6 +8,7 @@ import {
     userRegisterReducer,
     userDetailsReducer,
     userUpdateProfileReducer,
+    userListReducer,
 }
     from './reducers/userReducers'
 import { 
@@ -25,10 +26,12 @@ const reducer = combineReducers({
     userRegister: userRegisterReducer,
     userDetails: userDetailsReducer,
     userUpdateProfile: userUpdateProfileReducer,
+    userList: userListReducer,
     orderCreate: orderCreateReducer,
     orderDetails: orderDetailsReducer,
     orderPay: orderPayReducer,
     orderListMy: orderListMyReducer,
+    
 })
 
 const cartItemesFromStorage = localStorage.getItem('cartItems') ?
