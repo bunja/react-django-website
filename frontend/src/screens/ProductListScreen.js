5f3058a2a37564d911decd9ebdb54ee014dff0d0
Larisa Kosareva <kos.lv@yandex.com>
2022-04-20 11:56:33 +0200

    admit product list added

---
+++ frontend/src/screens/ProductListScreen.js
@@ -0,0 +1,105 @@
+import React, { useState, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { useParams, useNavigate } from 'react-router-dom'
+import { Table, Button, Row, Col } from 'react-bootstrap'
+import { LinkContainer } from 'react-router-bootstrap'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
+import { listProducts } from '../actions/productActions'
+
+
+function ProductListScreen() {
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products} = productList
+
+    const userLogin = useSelector(state => state.userLogin)
+    const { userInfo } = userLogin
+
+   
+    console.log('LIST Product SCREAM userList', productList)
+
+    useEffect(() => {
+        if(userInfo && userInfo.isAdmin){
+            dispatch(listProducts())
+        } else {
+            navigate('/login')
+        }
+    }, [dispatch, navigate, userInfo])
+
+    const deleteHandler = (id) => {
+        if (window.confirm('Are you sure yo want to delete this product?')){
+            // dispatch(deleteProduct(id))
+        }
+    }
+
+    const createProductHandler = (product) => {
+        //create product
+    }
+
+    return (
+        <div>
+            <Row className='align-items-center'>
+                <Col>
+                    <h1>Products</h1>
+                </Col>
+                <Col className='text-right'>
+                    <Button className='my-3'
+                            onClick={createProductHandler}>
+                        <i className='fas fa-plus'></i>Create Product
+                    </Button>
+                </Col>
+            </Row>
+            { loading 
+                ? (<Loader/>)
+                : error
+                    ? ( <Message variant='danger'>{error}</Message>)
+                    : (
+                        <Table striped bordered hover responsive className='table-sm'>
+                            <thead>
+                                <tr>
+                                <th>ID</th>
+                                <th>NAME</th>
+                                <th>PRICE</th>
+                                <th>CATEGORY</th>
+                                <th>BRAND</th>
+                                <th></th>
+                                </tr>
+                                
+                            </thead>
+
+                            <tbody>
+                                {products.map(product => (
+                                    <tr key={product._id}>
+                                        <td>{product._id}</td>
+                                        <td>{product.name}</td>
+                                        <td>{product.price}</td>
+                                        <td>{product.category}</td>
+                                        <td>{product.brand}</td>
+                                        <td>
+                                            <LinkContainer to={`/admin/product/${product._id}/edit`}>
+                                                <Button variant='light' className="btn-sm">
+                                                <i className='fas fa-edit'></i>
+                                                </Button>
+                                            </LinkContainer>
+                                            <Button variant='danger' 
+                                                    className="btn-sm"
+                                                    onClick={() => deleteHandler(product._id)}>
+                                                <i className='fas fa-trash'></i>
+                                            </Button>
+                                        </td>
+                                    </tr>
+                                ))}
+                            </tbody>
+
+                        </Table>
+                    )
+
+            }
+        </div>
+    )
+}
+
+export default ProductListScreen
